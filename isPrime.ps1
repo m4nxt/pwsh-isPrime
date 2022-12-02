5232a5957d8bc487565e60f8c1b5b82ff5f5d3a4
mathias <mathiasnextkbh@gmail.com>
2022-12-02 23:01:27 +0100

    Initial commit

---
+++ isPrime.ps1
@@ -0,0 +1,13 @@
+function isPrime ($n) {
+    if ( $n -eq 1 ) { return $False }
+    if ( $n -lt 4 ) { return $True }
+    if ( $n % 2 -eq 0 ) { return $False }
+    $m = 1 + [math]::Round([math]::Sqrt($n))
+    for ( $i = 3; $i -lt $m; $i += 1 ) {
+        if ( $n % $i -eq 0 ) { return $False }
+    }
+    return $True
+}
+
+[int]$userIn = Read-Host
+isPrime $userIn
